cf4baddb83ff47f3a1a3c4d959242007bdf406b5
Eduardo Moreno <hmitsurugi@gmail.com>
2020-08-10 00:23:52 +0200

    Progress.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class App():
 
     def render(self):
         # Draw background image
-        background  = pygame.image.load('resources/background.png')
+        background  = pygame.image.load('resources/background.png').convert()
         self.draw_tools['screen'].blit(background, (0, 0))
 
     def handle_events(self, events):
@@ -39,7 +39,6 @@ class Header():
         pygame.draw.rect(self.draw_tools['screen'], self.BACK_COLOR, (0, 0, 320, 18))
         pygame.draw.rect(self.draw_tools['screen'], self.BACK_COLOR, (2, 18, 316, 2))
         pygame.draw.lines(self.draw_tools['screen'], self.FRONT_COLOR, False, [(0, 0), (0,17), (2,19), (317,19), (319,17), (319,0)])
-
         textsurface = self.draw_tools['font'].render('Py States Backup', False, self.TEXT_COLOR)
         self.draw_tools['screen'].blit(textsurface,(10,3))
 
@@ -53,12 +52,9 @@ class Footer():
 
     def render(self):
         pygame.draw.rect(self.draw_tools['screen'], self.BACK_COLOR, (0, 225, 320, 15))
-
         textsurface = self.draw_tools['font'].render('exit', False, self.TEXT_COLOR)
-
         x = 320-10-textsurface.get_width()-15
         y = 227
-
-        start_icon  = pygame.image.load('resources/START_button.png')
+        start_icon  = pygame.image.load('resources/START_button.png').convert_alpha()
         self.draw_tools['screen'].blit(start_icon, (x, y))
         self.draw_tools['screen'].blit(textsurface,(x+15,y))
