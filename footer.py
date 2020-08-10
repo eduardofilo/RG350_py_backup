996000cfdc97a461f925ad58d7fe6a396b13f1d8
Eduardo Moreno <hmitsurugi@gmail.com>
2020-08-11 00:15:18 +0200

    Progress (buttons A and X in footer).

--- footer.py
+++ footer.py
@@ -33,10 +33,28 @@ def render():
         textsurface = settings.font.render('toggle', False, TEXT_COLOR)
         settings.screen.blit(textsurface,(x,y))
 
+        # Backup
+        x = x + textsurface.get_width() + 15
+        b_icon  = pygame.image.load('resources/B_button.png').convert_alpha()
+        settings.screen.blit(b_icon, (x, y))
+
+        x = x + b_icon.get_width() + 5
+        textsurface = settings.font.render('backup', False, TEXT_COLOR)
+        settings.screen.blit(textsurface,(x,y))
+
+        # Restore
+        x = x + textsurface.get_width() + 15
+        x_icon  = pygame.image.load('resources/X_button.png').convert_alpha()
+        settings.screen.blit(x_icon, (x, y))
+
+        x = x + x_icon.get_width() + 5
+        textsurface = settings.font.render('restore', False, TEXT_COLOR)
+        settings.screen.blit(textsurface,(x,y))
+
     # Exit
     start_icon  = pygame.image.load('resources/START_button.png').convert_alpha()
     textsurface = settings.font.render('exit', False, TEXT_COLOR)
-    
+
     x = 320 - start_icon.get_width() - textsurface.get_width() - 5 - 10
     y = 227
     settings.screen.blit(start_icon, (x, y))
