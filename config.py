f0fe5f5152020b1bbecf194bf45fbd785ca8f26c
Eduardo Moreno <hmitsurugi@gmail.com>
2020-08-11 11:52:53 +0200

    Progress (striping of config).

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ def init():
             items = line[:-1].split(SEPARATOR)
             if len(items) < 3:
                 raise Exception("Line %d in config file has insuficient items." % (n+1))
-            system = {'name': items[0], 'enabled': items[1] == 'True', 'dirs': []}
+            system = {'name': items[0].strip(), 'enabled': items[1].strip() == 'True', 'dirs': []}
             for i in range(2,len(items)):
-                if os.path.exists(items[i]):
-                    system['dirs'].append(items[i])
+                if os.path.exists(items[i].strip()):
+                    system['dirs'].append(items[i].strip())
             if len(system['dirs']) > 0:
                 settings.config.append(system)
             n = n + 1
